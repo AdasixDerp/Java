457051e7e6b8722303ed2329a9edbbf2abd2dd68
Adam <adamunnr@gmail.com>
2020-05-14 22:04:35 +0200

    Added Pomieszczenie

---
+++ Javanpz/src/javanpz/Pomieszczenie.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javanpz;
+
+/**
+ *
+ * @author Adasix
+ */
+public class Pomieszczenie extends ElementPietra {
+    public int szerokosc;
+    public int wysokosc;
+    public String nazwa;
+    
+    
+}
